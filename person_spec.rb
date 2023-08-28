ad54f052ab1e490a2fbf689bb86d9dc80c7cd6d9
Cosmas Wasswa <cosywas@gmail.com>
2023-08-28 11:28:47 +0300

    Fix linters

--- person_spec.rb
+++ person_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # class person and its methods
 class Person
   def initialize(age, name = 'Unknown', _parent_permission: true)
--- student_spec.rb
+++ student_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './person_spec'
 # class student inherite person class
 class Student < Person

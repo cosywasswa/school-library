a2003e73b1bc912fad9264ce990a0fd9d26de348
Cosmas Wasswa <cosywas@gmail.com>
2023-08-30 21:56:28 +0300

    Update teacher class variables

--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,11 @@
 require './person'
 # methods and variable for the class
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown')
-    super(age, name, true)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
+  attr_accessor :specialization
 
   def can_use_services?
     true

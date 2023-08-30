f1dd956e44bb6f29574d6b11787a78a55dc106e1
Cosmas Wasswa <cosywas@gmail.com>
2023-08-30 21:55:41 +0300

    Updated student class variables

--- student.rb
+++ student.rb
@@ -3,14 +3,14 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.includes?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky

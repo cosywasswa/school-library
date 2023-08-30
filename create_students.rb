f64a19e94367963c77320b55feccdfb560ae9142
Cosmas Wasswa <cosywas@gmail.com>
2023-08-30 21:52:15 +0300

    Add method for creating new student

---
+++ create_students.rb
@@ -0,0 +1,24 @@
+require './book'
+require './person'
+require './rental'
+require './student'
+require './teacher'
+require './classroom'
+
+# rubocop:disable Metrics/MethodLength
+def create_student
+  print 'Age: '
+  age = gets.chomp
+  print 'Name: '
+  name = gets.chomp
+  print 'Has parent permission? [Y/N] '
+  parent_permission = gets.chomp.downcase
+  if parent_permission == 'y'
+    @persons.push(Student.new(true, age, name))
+  else
+    @persons.push(Student.new(false, age, name))
+  end
+  puts 'Person created successfully'
+  list_options
+end
+# rubocop:enable Metrics/MethodLength

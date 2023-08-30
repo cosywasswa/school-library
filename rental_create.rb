e8ef0d9e8d90c7934855600a47bb214179ea0252
Cosmas Wasswa <cosywas@gmail.com>
2023-08-30 23:15:37 +0300

    Refactor to simplify code

--- rental_create.rb
+++ rental_create.rb
@@ -7,26 +7,14 @@ require './classroom'
 require './app'
 
 def create_rental
-  if @books.empty?
-    puts 'No books in store'
-    puts 'select 4 to create a new book'
-    get_options
-  else
     puts 'Select a book from the following list by number'
     @books.each_with_index do |book, index|
       puts "#{index}) Title: #{book.title}, Author: #{book.author}"
-    end
     book_index = gets.chomp.to_i
-
-    if @persons.empty?
-      puts 'No people in records'
-      puts 'select 3 to enter a new person'
-      get_options
-    else
       puts 'Select a person from the following list by number'
       @persons.each_with_index do |person, index|
         puts "#{index}) Name: \"#{person.name}\", ID: #{person.id}, Age: #{person.age}"
-      end
+    
     end
     person_index = gets.chomp.to_i
 
